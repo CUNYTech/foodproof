80f6e3755d9107a559e713c96dd05ccd19e558ed
Binod Timalsina <t17711@users.noreply.github.com>
2017-02-28 13:01:34 -0500

    Create sql_script.sql

---
+++ sql_script.sql
@@ -0,0 +1,22 @@
+use foodproof_users;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` datetime,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10; 
+
+alter table `users` add constraint `name` unique (`name`); 
+alter table `users` add constraint `email` unique (`email`); 
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `user` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  'created_at' datetime,
+  PRIMARY KEY (`token`)
+)
