aae93242c4d10e309375bb304a1aeeccd9ceca79
Brian Hernandez <Brian@Hernandez-cs.com>
2017-03-01 19:32:37 -0500

    POST edited

--- app/src/main/java/edu/cuny/foodproof/Login.java
+++ app/src/main/java/edu/cuny/foodproof/Login.java
@@ -69,6 +69,56 @@ public class Login extends AppCompatActivity  implements View.OnClickListener{
         }
     }
 
+    private class makePostRequest extends AsyncTask<String, Void, String>{
 
+      @Override
+      protected String doInBackground(){
+        String inUsername = etUsername.getText().toString();
+        String inPassword = etPassword.getText().toString();
+        String postParameters = "username=" + inUsername + "&password=" + inPassword;
+        try {
+            URL url = new URL(params[0]);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setInstanceFollowRedirects(false);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+            urlConnection.setRequestProperty( "charset", "utf-8");
+            urlConnection.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+            urlConnection.setUseCaches(false);
+            try(DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream())){
+                wr.write(postData);
+                wr.flush();
+                wr.close();
+            }
+            int responseCode = urlConnection.getResponseCode();
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            final StringBuilder output = new StringBuilder("Request URL " + params[0]);
+            output.append(System.getProperty("line.separator") + "Request Parameters " + urlParameters);
+            output.append(System.getProperty("line.separator")  + "Response Code " + responseCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = "";
+            StringBuilder responseOutput = new StringBuilder();
+            System.out.println("output===============" + br);
+            while((line = br.readLine()) != null ) {
+              responseOutput.append(line);
+            }
+            br.close();
+            output.append(System.getProperty("line.separator") + "Response " + System.getProperty("line.separator") + System.getProperty("line.separator") + responseOutput.toString());
+
+        }
+        catch(IOException e){
+            throw new RuntimeException(e);
+        }
+      }
+
+      @Override
+      protected void onPostExecute(String result){
+
+      }
+
+
+
+    }
 
 }
