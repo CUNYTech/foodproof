9209b9bbaaf6675104046bb6a345cc16337d928b
t17711 <btm28657@hotmail.com>
2017-03-03 11:26:17 -0500

    standardized error handling, created route controller for route we didnot want

--- function/database.php
+++ function/database.php
@@ -2,77 +2,36 @@
   require_once('db_credentials.php');
 
   function db_connect(&$error) {
-    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-    if(mysqli_connect_errno()) {
-      $msg = "Database connection failed: ";
-      $msg .= mysqli_connect_error();
-      $msg .= " (" . mysqli_connect_errno() . ")";
-      $error['connection Error'] = $msg;
-    exit;
-	}
-    return $connection;
+      $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+      if(mysqli_connect_errno()) {
+        $msg = "Database connection failed: ";
+        $msg .= mysqli_connect_error();
+        $msg .= " (" . mysqli_connect_errno() . ")";
+        $error['connection Error'] = $msg;
+        return false;
+    }
+      return $connection;
   }
 
   function db_query($connection, $sql, &$error) {
     	$result_set = mysqli_query($connection, $sql);
-	if($result_set==false){
-		$error['SQL error']=mysqli_error($connection);
-		return false;
-	}
-	else{
-		return $result_set;
-	}
+      if($result_set==false){
+        $error['SQL error']=mysqli_error($connection);
+        return false;
+      }
+      else{
+        if(substr($sql, 0, 7) == 'SELECT ') {
+            confirm_query($result_set,$error);
+        }
+      }
+      return $result_set;
   }
 
-
-  function add_user($name, $password, $email,$db, &$error){
-    $created_at = date("Y-m-d H:i:s");
-    $sql = "INSERT INTO users";
-    $sql .= "(name,email,password,created_at) ";
-    $sql .= "VALUES (";
-    $sql .= "'" . db_escape($db, $name) . "',";
-    $sql .= "'" . db_escape($db, $email). "',";
-    $sql .= "'" . db_escape($db, $password) . "',";
-    $sql .= "'" . $created_at . "'";
-    $sql .= ");";
-
-    // For INSERT statements, $result is just true/false
-    $result = db_query($db, $sql, $error);
-    
-    if(!$result){return false;}
-    else{return true;}
-
-
-
+function confirm_query($result_set,&$error) {
+    if(!$result_set) {
+      $error["error"]["DB"]="Database query failed";
+    }
   }
-
- function login_user($name,$password,$db,&$error){
-	$sql = "SELECT name FROM users ";
-    	$sql .= "WHERE (name='" . db_escape($db,$name) . "') and(password='" . db_escape($db,$password) . "');";
-    	$result = db_query($db, $sql,$error);
-	if (mysqli_num_rows($result)==1){
-		return true;
-	}
-
-	return false;
- }
-
- function create_token($user,$token,$db,&$error){
-    $created_at = date("Y-m-d H:i:s");
-    $sql = "INSERT INTO sessions ";
-    $sql .= "(user,token,created_at) ";
-    $sql .= "VALUES (";
-    $sql .= "'" . db_escape($db, $user) . "',";
-    $sql .= "'" . db_escape($db, $token). "',";
-    $sql .= "'" . $created_at . "'";
-    $sql .= ");";
-	//echo $sql;
-    // For INSERT statements, $result is just true/false
-    $result = db_query($db, $sql,$error);
-    return $result;
-
-	
- }
 
   function db_fetch_assoc($result_set) {
     return mysqli_fetch_assoc($result_set);
--- public/index.php
+++ public/index.php
@@ -15,92 +15,172 @@ $configuration = [
 $c = new \Slim\Container($configuration);
 $app = new \Slim\App($c);
 
-// manage registration
-$app->post("/test",function($request, $response,$args) {
-	$data = $request->getParsedBody();
-	$test=[];
-	$test['text'] = filter_var($data['text'], FILTER_SANITIZE_STRING);
+/****************************************************************************
+			/register
+//////////////////////////////////////////////////////////////////////////////
 
-	$test_out = json_encode($test);
-	$response = $response->withHeader('Content-Type', 'application/json');
-	$response->getBody()->write($test_out);
-	return $response;
-
-});
+function reads post request on /register path 
+then it looks for parameters we need
+if we dont find them then it throws error
+if we do then it imports database file and creaes connection
+if connection succeeds then it imports query file and registers user to db
+*****************************************************************************
+input: POST "user","email","password" on /register
+output: Content-Type', 'application/json file
+		if succeeds then returns json string
+			{"result": "succeed"}
+		is fails then
+			{"Error":{the list of error}, "Result": "failed"}
+///////////////////////////////////////////////////////////////////////////////
+******************************************************************************/
 
 $app->post('/register', function ($request, $response,$args) {
-	require_once '../function/database.php';
-	$error=[];
-	$error["Result"]= "failed";	
-	$db=db_connect($error);
-
 	// this is to read post value
 	$data = $request->getParsedBody();
 
-	//read names from post
-	$name= filter_var($data['user'],FILTER_SANITIZE_STRING);	
-	$password = filter_var($data['password'],FILTER_SANITIZE_STRING);
-	$email = filter_var($data['email'],FILTER_SANITIZE_STRING);
+	// create error array
+	$error=[];
 	
-	// add user
-	$add = add_user($name,$password,$email,$db,$error);
+	//check if get required 
+	if(!isset($data['user'])){$error["Error"]["username"]="not entered";}
+	if(!isset($data['password'])){$error["Error"]["password"]="not entered";}
+	if(!isset($data['email'])){$error["Error"]["email"]= "not entered";}
 	
-   	if($add){
-		$out= json_encode(["Result" => "succeed"]);
-    		$response->getBody()->write($out);
-	}
-	else{
-		$out= json_encode($error);
-    		$response->getBody()->write($out);
-	}
+	// if no error continue
+	if(sizeof($error)==0){
+			// connect db
+			require_once '../function/database.php';
+			$db=db_connect($error);
 
+			if($db){
+				require_once '../function/user_query_functions.php';
+			// sanitize
+			$name= filter_var($data['user'],FILTER_SANITIZE_STRING);
+			$password = filter_var($data['password'],FILTER_SANITIZE_STRING);
+			$email = filter_var($data['email'],FILTER_SANITIZE_STRING);
+			
+			// add user
+			$add = add_user($name,$password,$email,$db,$error);
+			
+			// if no error respond
+			if(sizeof($error)==0){
+				$out= json_encode(["Result" => "succeed"]);
+				$response->getBody()->write($out);
+			}
+			
+			// close connection
+			db_close($db);
+		}
+	}
+	// check error
+	if (sizeof($error)>0){
+		$error["Result"]="failed";
+		$out= json_encode($error,JSON_FORCE_OBJECT);
+		$response->getBody()->write($out);
+	}
 	$response = $response->withHeader('Content-Type', 'application/json');
-	db_close($db);
     return $response;
 });
 
-// manage login
+
+/****************************************************************************
+				/login
+//////////////////////////////////////////////////////////////////////////////
+
+function reads post request on /login path 
+then it looks for parameters we need
+if we dont find them then it throws error
+if we do then it imports database file and creaes connection
+if connection succeeds then it imports query file and checks if user password combination exists
+if it exists then it generates a random token and stores (tokem user) info to db
+then it returns token
+*****************************************************************************
+input: POST "user","password" required on /login
+output: Content-Type', 'application/json file
+		if succeeds then returns json string
+			{"result": "succeed", "token":token value}
+		is fails then
+			{"Error":{the list of error}, "Result": "failed"}
+///////////////////////////////////////////////////////////////////////////////
+******************************************************************************/
+
+
 $app->post('/login', function ($request, $response,$args) {
-	require_once '../function/database.php';
-	$error=[];
-	//dedfalult error
-	$error["Result"]="failed";
-	$db=db_connect($error);
-
 	// this is to read post value
 	$data = $request->getParsedBody();
 
-	//read names from post
-	$name= filter_var($data['user'],FILTER_SANITIZE_STRING);	
-	$password = filter_var($data['password'],FILTER_SANITIZE_STRING);
+	// create error array
+	$error=[];
 
-	// login
-	$log = login_user($name,$password,$db,$error);
-	if($log){
-		$token = base64_encode(openssl_random_pseudo_bytes(20)); 
-		$result = create_token($name,$token,$db,$error);
-		if ($result){
-			$out= json_encode(["Result" => "succeed", "Token" => $token]);
-    			$response->getBody()->write($out);
+	//check if exists
+	if(!isset($data['user'])){$error["Error"]["username"]="not entered";}
+	if(!isset($data['password'])){$error["Error"]["password"]="not entered";}
+	
+	
+	// check if error happened
+	if (sizeof($error)==0){
+		require_once '../function/database.php';
+		$db=db_connect($error);
+
+		if($db){
+			require_once '../function/user_query_functions.php';
+
+			//read names from post
+			$name= filter_var($data['user'],FILTER_SANITIZE_STRING);	
+			$password = filter_var($data['password'],FILTER_SANITIZE_STRING);
+
+			// login
+			login_user($name,$password,$db,$error);
+
+			// if no error, respond
+			if (sizeof($error)==0){
+
+				// generate token
+				$token = base64_encode(openssl_random_pseudo_bytes(20)); 
+				create_token($name,$token,$db,$error);
+
+				// if no error continue
+				if (sizeof($error)==0){
+					$out= json_encode(["Result" => "succeed", "Token" => $token]);
+					$response->getBody()->write($out);
+				}
+			}
+			// close connection
+			db_close($db);
 		}
-		else{
-			$response->getBody()->write(json_encode($error));
-
-		}
+	
 	}
-	else{
-		$out= json_encode($error);
-    		$response->getBody()->write($out);
+	
+	// check if error happened
+	if (sizeof($error)>0){
+		$error["Result"]="failed";
+		$out= json_encode($error,JSON_FORCE_OBJECT);
+		$response->getBody()->write($out);
 	}
-
+	
 	$response = $response->withHeader('Content-Type', 'application/json');
-	db_close($db);
     return $response;
 });
 
-// do default error
-$app->get('/', function (Request $req,  Response $res, $args = []) {
-    return $res->withStatus(400)->write('Error in parsing');
+/****************************************************************************
+				/*
+//////////////////////////////////////////////////////////////////////////////
+
+function reads any HTTP request on site we didnot define 
+*****************************************************************************
+input: any request we did not define
+output: Content-Type', 'application/json file with status 400
+			{"Result":"failed","Error":{"path":"invalid"}}
+
+///////////////////////////////////////////////////////////////////////////////
+******************************************************************************/
+$app->any('/[{path:.*}]', function($request, $response, $path = null) {
+	$error["Result"]="failed";
+	$error["Error"]["path"]="invalid";	
+	$out= json_encode($error,JSON_FORCE_OBJECT);
+	$response->getBody()->write($out);
+	$response = $response->withHeader('Content-Type', 'application/json');
+    return $response->withStatus(400);
 });
 
 $app->run();
