a9c2662ae07d273908fff38ca2844a11cdd504aa
t17711 <btm28657@hotmail.com>
2017-03-09 20:13:03 -0500

    checked user exists before querying in the db

--- function/query/user_ingredient_functions.php
+++ function/query/user_ingredient_functions.php
@@ -18,16 +18,21 @@ function add_ingredient($user,$ingredient,$db, &$error){
       if(sizeof($error)!=0) return false;
   }
 
+
  function user_ingredient_list($user,$num,$db,&$error){
       // get uid
       $user_id=get_user_id_by_name($user,$db,$error);
+      if(sizeof($error)!=0) return false;
 
       // get list of ingredient uid
       $ingredient_list=[];
       $ingredient_result = get_ingredient_id_by_user_id($user_id,$num,$db,$error);
+      if(sizeof($error)!=0) return false;
 
       while($ingredient=db_fetch_assoc($ingredient_result) ){
           $ingredient_list[] = get_ingredient_name_by_id($ingredient['ingredient_id'],$db,$error);
+          if(sizeof($error)!=0) return false;
+
       }
       return $ingredient_list;
   }
