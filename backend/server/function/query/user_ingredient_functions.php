d6dc2702ea5d2edd249c25c3845c3cf4440de051
binod <btm28657@hotmail.com>
2017-03-22 21:23:07 -0400

    made it so that only the same user cant have duplicate iterm

--- backend/server/function/query/user_ingredient_functions.php
+++ backend/server/function/query/user_ingredient_functions.php
@@ -3,21 +3,31 @@ declare(strict_types=1);
 
 include_once INGREDIENT;
 
-function add_ingredient($user,$ingredient,$db, &$error){
+function add_ingredient($user,$ingredient,$db, &$error): int{
       // get uid
       $user_id=get_user_id_by_name($user,$db,$error);
-      if(sizeof($error)!=0) return false;
+      if(sizeof($error)!=0) return -1;
+     
+      $ingredient_id=0;
+      
+      // check if ingredient already exists
+      $dummy_error=[];
+      $ingredient_id=get_ingredient_id_by_name($ingredient,$db,$dummy_error);
 
-      // insert ingredient to table and get its uid
-      insert_to_ingredient_table($ingredient,$db,$error);      
-      if(sizeof($error)!=0) return false;
+      // if ingredient doesnot exist yet add it
+      if(sizeof($dummy_error)!=0){
+        // insert ingredient to table and get its uid
+        insert_to_ingredient_table($ingredient,$db,$error);      
+        if(sizeof($error)!=0) return -1;
 
-      // get last autoincrement id
-      $ingredient_id =  db_insert_id($db);
+        // get last autoincrement id
+        $ingredient_id =  db_insert_id($db);
+      }
 
       //insert to user-ingretdient tuple table
       insert_to_user_ingredient($user_id,$ingredient_id,$db,$error);      
-      if(sizeof($error)!=0) return false;
+      if(sizeof($error)!=0) return -1;
+      
       return $ingredient_id;
   }
 
@@ -37,5 +47,6 @@ function add_ingredient($user,$ingredient,$db, &$error){
           if(sizeof($error)!=0) return NULL;
 
       }
+
       return $ingredient_list;
   }
