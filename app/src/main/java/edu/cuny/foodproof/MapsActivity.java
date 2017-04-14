9901053efac598d300b2ea9d7776d9dcdc759f9d
Barindra Narinesingh <Barindra.Narinesingh@gmail.com>
2017-04-14 04:28:38 -0400

    Still maps. gps still not working, or is it not sure
    too late for this

--- app/src/main/java/edu/cuny/foodproof/MapsActivity.java
+++ app/src/main/java/edu/cuny/foodproof/MapsActivity.java
@@ -1,9 +1,13 @@
 package edu.cuny.foodproof;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -15,6 +19,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
@@ -23,6 +28,10 @@ import java.util.List;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Marker mMarker;
+    LocationManager lm;
+    double lat, lng;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
+    LocationListener listener = new LocationListener() {
+        public void onLocationChanged(Location loc) {
+            LatLng coordinate = new LatLng(loc.getLatitude(), loc.getLongitude());
+            lat = loc.getLatitude();
+            lng = loc.getLongitude();
+
+            if(mMarker != null)
+                mMarker.remove();
+
+            mMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 16));
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+        public void onProviderEnabled(String provider) {}
+        public void onProviderDisabled(String provider) {}
+    };
 
     public void onMapSearch(View view) {
         EditText locationSearch = (EditText) findViewById(R.id.editText);
@@ -66,20 +93,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
     }
 
-    public void onHybridMap(View view) {
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+   public void onResume() {
+        super.onResume();
+
+        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        boolean isNetwork = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        boolean isGPS = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if(isNetwork) {
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10, listener);
+            Location loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if(loc != null) {
+                lat = loc.getLatitude();
+                lng = loc.getLongitude();
+            }
+        }
+
+        if(isGPS) {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, listener);
+            Location loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(loc != null) {
+                lat = loc.getLatitude();
+                lng = loc.getLongitude();
+            }
+        }
     }
+
+    public void onPause() {
+        super.onPause();
+        lm.removeUpdates(listener);
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(40.7128, 74.0059);
+        LatLng sydney = new LatLng(40.730610, -73.935242);
         mMap.addMarker(new MarkerOptions().position(sydney).title("New York City, New York"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         mMap.setMyLocationEnabled(true);
+
     }
+
+
 }
