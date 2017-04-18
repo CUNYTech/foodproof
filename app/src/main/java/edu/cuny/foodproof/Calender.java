24157e632f7daf368964d2ff22bd8aa12cac2994
Barindra Narinesingh <Barindra.Narinesingh@gmail.com>
2017-04-17 20:39:06 -0400

    Cal droid working
    see title

--- app/src/main/java/edu/cuny/foodproof/Calender.java
+++ app/src/main/java/edu/cuny/foodproof/Calender.java
@@ -2,6 +2,7 @@ package edu.cuny.foodproof;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
@@ -20,6 +21,10 @@ import android.view.MenuItem;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Calendar;
+
 public class Calender extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,16 @@ public class Calender extends AppCompatActivity {
         //Standard onCreate() procedures
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.add(R.id.Calender, caldroidFragment).commit();
     }
 }
 
