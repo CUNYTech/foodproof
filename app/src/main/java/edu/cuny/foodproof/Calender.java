67ce8a43f440b70c9d068c6916332203ea911732
Barindra Narinesingh <Barindra.Narinesingh@gmail.com>
2017-04-17 21:06:35 -0400

    added red to date
    cal updaated

--- app/src/main/java/edu/cuny/foodproof/Calender.java
+++ app/src/main/java/edu/cuny/foodproof/Calender.java
@@ -1,7 +1,9 @@
 package edu.cuny.foodproof;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentTransaction;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -24,8 +26,12 @@ import android.widget.Toast;
 import com.roomorama.caldroid.CaldroidFragment;
 
 import java.util.Calendar;
+import java.util.Date;
+
+import static edu.cuny.foodproof.R.drawable.default_button;
 
 public class Calender extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -39,9 +45,12 @@ public class Calender extends AppCompatActivity {
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
         args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
         caldroidFragment.setArguments(args);
+        caldroidFragment.setBackgroundDrawableForDate(getDrawable(R.drawable.default_button), new Date(System.currentTimeMillis()));
 
         FragmentTransaction t = getSupportFragmentManager().beginTransaction();
         t.add(R.id.Calender, caldroidFragment).commit();
+
+
     }
 }
 
