5269a76c1545b6fb9c82c7c74148ddbf90c1ec63
Barindra Narinesingh <Barindra.Narinesingh@gmail.com>
2017-04-17 21:25:45 -0400

    Added string declare to calender
    see title

--- app/src/main/java/edu/cuny/foodproof/Calender.java
+++ app/src/main/java/edu/cuny/foodproof/Calender.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
@@ -24,6 +25,9 @@ import android.widget.CalendarView;
 import android.widget.Toast;
 
 import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
+
+import org.w3c.dom.Text;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -31,6 +35,9 @@ import java.util.Date;
 import static edu.cuny.foodproof.R.drawable.default_button;
 
 public class Calender extends AppCompatActivity {
+    CaldroidFragment caldroidFragment;
+    FragmentTransaction t;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +46,22 @@ public class Calender extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
 
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        caldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
         Calendar cal = Calendar.getInstance();
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
         args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
         caldroidFragment.setArguments(args);
-        caldroidFragment.setBackgroundDrawableForDate(getDrawable(R.drawable.default_button), new Date(System.currentTimeMillis()));
+        caldroidFragment.setCaldroidListener(new CaldroidListener() {
+            @Override
+            public void onSelectDate(Date date, View view) {
+                Toast.makeText(getApplicationContext(), date.toString(), Toast.LENGTH_SHORT).show();
+                TextView tView = (TextView) view;
+                tView.setText("My Recipe");
+            }
+        });
 
-        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t = getSupportFragmentManager().beginTransaction();
         t.add(R.id.Calender, caldroidFragment).commit();
 
 
